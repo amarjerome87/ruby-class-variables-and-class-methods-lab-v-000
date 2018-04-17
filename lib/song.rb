632ac7c95901d081eb124c3ce8d33403cbf691df
Amar Chatman <amarjerome@gmail.com>
2018-04-17 17:49:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,4 +40,15 @@ def self.count
     return genre_hash
   end
 
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash[artist]
+        artist_hash[artist] += 1
+      else
+        artist_hash[artist] = 1
+      end
+    end
+    return artist_hash
+
 end
