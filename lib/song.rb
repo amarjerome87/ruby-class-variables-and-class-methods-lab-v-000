b5cff895e457454cdb124784edf324f636f429fc
Amar Chatman <amarjerome@gmail.com>
2018-04-17 16:58:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ def self.count
   def self.genre_count
     genre_hash = {}
     @@genres.each do |genre|
-    genre_hash[genre] = @@genres.join(",").count
+    genre_hash[genre] = self.genres.count
     end
     return genre_hash
     end
